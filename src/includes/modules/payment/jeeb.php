3535dcb36868cefa1ba190f7268a44d751c039ad
R. Giridhar <gdhar67@gmail.com>
2019-03-14 23:53:56 +0530

    adding TOMAN currency option

--- src/includes/modules/payment/jeeb.php
+++ src/includes/modules/payment/jeeb.php
@@ -48,6 +48,9 @@ class jeeb extends base
       case 'IRR':
         $this->base_cur = "irr";
         break;
+      case 'TOMAN':
+        $this->base_cur = "toman";
+        break;
       case 'USD':
         $this->base_cur = "usd";
         break;
@@ -138,8 +141,14 @@ class jeeb extends base
     $baseCur   = $this->base_cur;
     $targetCur = $this->target_cur;
     $lang      = $this->lang;
+    $order_total= $order->info['total'];
 
-    $amount = \Jeeb\Merchant\Order::convertBaseToTarget($baseUri, $order->info['total'], $signature, $baseCur);
+    if($baseCur=='toman'){
+      $baseCur='irr';
+      $order_total *= 10;
+    }
+
+    $amount = \Jeeb\Merchant\Order::convertBaseToTarget($baseUri, $order_total, $signature, $baseCur);
     $this->log('Signature ='.$signature.' base Url = '.$baseUri.' Convertion = '.$amount, true);
 
     $params = array(
@@ -152,6 +161,8 @@ class jeeb extends base
       "allowTestNet"     => MODULE_PAYMENT_JEEB_TEST == "True" ? true : false,
       "language"         => $lang
     );
+
+
 
     $token = \Jeeb\Merchant\Order::createInvoice($baseUri, $amount, $params, $signature);
     $this->log('Token = '.$token.' Params ='.$params['requestAmount'], true);
@@ -186,13 +197,13 @@ class jeeb extends base
     }
 
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values('Enable Jeeb Module', 'MODULE_PAYMENT_JEEB_STATUS', 'False', 'Enable the Jeeb bitcoin plugin?', '6', '0', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values('Select Language', 'MODULE_PAYMENT_JEEB_LANG', 'False', 'Select the language of the Jeeb\'s payment page.', '6', '8', 'zen_cfg_select_option(array(\'Auto-Select\', \'English\', \'Persian\'), ', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values('Jeeb Signature', 'MODULE_PAYMENT_JEEB_API_KEY', '0', 'Your Jeeb Signature', '6', '0', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values('Sort order of display.', 'MODULE_PAYMENT_JEEB_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '8', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values('Enable test mode?', 'MODULE_PAYMENT_JEEB_TEST', 'False', 'Enable test mode to test your configuration and user experience.', '6', '1', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values('Select Base currency', 'MODULE_PAYMENT_JEEB_BASE_CUR', 'False', 'Select the currency of your site.', '6', '1', 'zen_cfg_select_option(array(\'BTC\', \'IRR\', \'EUR\', \'USD\'), ', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values('Select Base currency', 'MODULE_PAYMENT_JEEB_BASE_CUR', 'False', 'Select the currency of your site.', '6', '1', 'zen_cfg_select_option(array(\'BTC\', \'IRR\', \'EUR\', \'TOMAN\', \'USD\'), ', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values('Select Target currency', 'MODULE_PAYMENT_JEEB_TARGET_CUR', 'False', 'The target currency to which your base currency will get converted.', '6', '1', 'zen_cfg_select_multioption(array(\'BTC\', \'XRP\', \'ETH\', \'XMR\', \'BCH\', \'LTC\', \'TEST-BTC\'), ', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, use_function, date_added) values('Set Pending Order Status', 'MODULE_PAYMENT_JEEB_PENDING_STATUS_ID', '" . intval(DEFAULT_ORDERS_STATUS_ID) .  "', 'Status in your store when Jeeb Invoice status is pending.<br />(\'Pending\' recommended)', '6', '5', 'zen_cfg_pull_down_order_statuses(', 'zen_get_order_status_name', now())");
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values('Select Language', 'MODULE_PAYMENT_JEEB_LANG', 'False', 'Select the language of the Jeeb's payment page.', '6', '1', 'zen_cfg_select_option(array(\'Auto-Select\', \'Englis\', \'Persian\'), ', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, use_function, date_added) values('Set Awaiting Order confimation Status', 'MODULE_PAYMENT_JEEB_PAID_STATUS_ID', '2', 'Status in your store when Jeeb Invoice status is paid and awaiting confirmation from bitcoin network.<br />(\'Awaiting Confirmation\' recommended)', '6', '6', 'zen_cfg_pull_down_order_statuses(', 'zen_get_order_status_name', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, use_function, date_added) values('Set Paid Order Status', 'MODULE_PAYMENT_JEEB_CONFIRMED_STATUS_ID', '2', 'Status in your store when Jeeb confirms the Invoice payment.<br />(\'Confirmed\' recommended)', '6', '6', 'zen_cfg_pull_down_order_statuses(', 'zen_get_order_status_name', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, use_function, date_added) values('Set Expired Order Status', 'MODULE_PAYMENT_JEEB_EXPIRED_STATUS_ID', '7', 'Status in your store when Jeeb Invoice status is expired.<br />(\'Expired\' recommended)', '6', '6', 'zen_cfg_pull_down_order_statuses(', 'zen_get_order_status_name', now())");
